b06f453cb2cdd57a84a14f3d09f5d947d0907839
jchan34 <jeffreychan34@gmail.com>
2020-08-03 13:30:02 -0700

    Made general function to getGetCan()

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void motorStop();
 void handle_R_interrupt();
 void handle_L_interrupt();
 void depositCans();
-
+void goGetCan(int pivot_count, int drive_count, int backup_count);
 void openGate();
 void closeGate();
 void funMode();
@@ -22,7 +22,8 @@ SensorArray sensor_array = SensorArray();
 Encoders encoders = Encoders(sensor_array);
 Sonar_Logic sl = Sonar_Logic(&encoders);
 int pivot_count = 0;
-
+double start_time;
+double check_time;
 void setup()
 {
   pinSetup();
@@ -63,6 +64,7 @@ void setup()
   {
     funMode();
   }
+  start_time = millis();
 
   //grab bin and pivot to tape
   encoders.backup(4, 4);
@@ -92,11 +94,6 @@ void loop()
   display.println(error);
   display.display();
 
-  // int g = map(analogRead(POT), 0, 1023, 0, 150);
-  // pid.Kp = g;
-  // display.println(pid.Kp);
-  // display.println(" ");
-
   // follow tape
   pid.calculatePID(error);
   motorPIDcontrol(BASE_SPEED);
@@ -105,79 +102,60 @@ void loop()
   {
     pivot(RIGHT, PIVOT_SPEED);
   }
-  if (sl.pollSonar() < SONAR_LIMIT_FAR)
+  if (sl.pollSonar() < SONAR_LIMIT_FARTHEST)
   {
-    if (sl.pollSonar() < SONAR_LIMIT_FAR)
+    if (sl.pollSonar() < SONAR_LIMIT_FARTHEST)
     {
 
-      if (sl.pollSonar() < SONAR_LIMIT_CLOSE)
+      if (sl.pollSonar() < SONAR_LIMIT_CLOSEST)
       {
-        //small backwards movement
-        encoders.adjustmentBackupCount(10);
-        //open gate and small pivot
-        openGate();
-        encoders.rightPivotCount(14);
-
-        //drive straight and close
-        encoders.drive(24, 24);
-
-        closeGate();
-        //deposit
-
-        depositCans();
-        //pivot left
-        encoders.backup(24, 24);
-        pivot(LEFT, PIVOT_SPEED);
+        goGetCan(14, 14, 10);
+      }
+      else if (sl.pollSonar() < SONAR_LIMIT_CLOSE)
+      {
+        goGetCan(14, 17, 10);
       }
       else if (sl.pollSonar() < SONAR_LIMIT_MID)
       {
-
-        //small backwards movement
-        encoders.adjustmentBackupCount(10);
-        //open gate and small pivot
-        openGate();
-        encoders.rightPivotCount(18);
-
-        //drive straight and close
-        encoders.drive(43, 43);
-
-        closeGate();
-
-        //deposit
-        depositCans();
-
-        if (!digitalRead(FUNSWITCH))
-        {
-          funMode();
-        }
-
-        //pivot left
-        encoders.backup(43, 43);
-        pivot(LEFT, PIVOT_SPEED);
+        goGetCan(14, 20, 10);
       }
 
       else if (sl.pollSonar() < SONAR_LIMIT_FAR)
       {
-        //small backwards movement
-        encoders.adjustmentBackupCount(10);
-        //open gate and small pivot
-        openGate();
-        encoders.rightPivotCount(23);
+        goGetCan(14, 24, 10);
+      }
+      else if (sl.pollSonar() < SONAR_LIMIT_FARTHEST)
+      {
+        goGetCan(14, 27, 10);
+      }
+      check_time = millis();
 
-        //drive straight and close
-        encoders.drive(62, 62);
-
-        closeGate();
-
-        //deposit
+      if (check_time - start_time > 58000)
+      {
         depositCans();
-
-        //pivot left
-        encoders.backup(62, 62);
-        pivot(LEFT, PIVOT_SPEED);
       }
     }
   }
+}
+
+void goGetCan(int pivot_count, int drive_count, int backup_count)
+{
+  //small backwards movement
+  encoders.adjustmentBackupCount(backup_count);
+  //open gate and small pivot
+  openGate();
+  encoders.rightPivotCount(pivot_count);
+
+  //drive straight and close
+  encoders.drive(drive_count, drive_count);
+
+  closeGate();
+  //deposit
+
+  depositCans();
+  //pivot left
+  encoders.backup(drive_count, drive_count);
+  pivot(LEFT, BASE_SPEED);
 }
 
 void motorPIDcontrol(int speed)
